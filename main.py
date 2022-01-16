affb85d8583d9242755b6c7af2ee70bd6363d839
Marian Lingsch Rosenfeld <mrlingsch@gmail.com>
2022-01-16 21:23:59 +0100

    Improved code formatting

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import warnings
-from qiskit import QuantumCircuit, Aer
-import matplotlib.pyplot as plt
 import os
+import warnings
+
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, Aer
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def diffusion_operator(quantum_circuit, amnt_qubits):
@@ -19,11 +22,13 @@ def diffusion_operator(quantum_circuit, amnt_qubits):
         quantum_circuit.h(qubit)
     return quantum_circuit
 
+
 def all_one_oracle(quantum_circuit, amnt_non_ancilliary_qubits, _amnt_ancilliary_qubits):
     quantum_circuit.mcx(list(range(amnt_non_ancilliary_qubits)), amnt_non_ancilliary_qubits)
     quantum_circuit.cz(amnt_non_ancilliary_qubits, 0)
     quantum_circuit.mcx(list(range(amnt_non_ancilliary_qubits)), amnt_non_ancilliary_qubits)
     return quantum_circuit
+
 
 def write_result(quantum_circuit, postfix, amount_non_ancilliary_qubits):
     dir = "output"
@@ -31,7 +36,7 @@ def write_result(quantum_circuit, postfix, amount_non_ancilliary_qubits):
         os.makedirs(dir)
     simulator = Aer.get_backend('statevector_simulator')
     result_state = simulator.run(quantum_circuit).result().get_statevector()
-    real_part_of_result = list(map(lambda x: x.real, result_state))[:2**amount_non_ancilliary_qubits]
+    real_part_of_result = list(map(lambda x: x.real, result_state))[:2 ** amount_non_ancilliary_qubits]
     plt.bar(range(len(real_part_of_result)), real_part_of_result)
     plt.xlabel('Real part of state phase')
     plt.ylabel('State')
@@ -40,8 +45,9 @@ def write_result(quantum_circuit, postfix, amount_non_ancilliary_qubits):
     plt.close()
     return
 
+
 def grover(oracle):
-    iterations = 30
+    iterations = 1
     amnt_non_anciliary_qubits = 4
     amnt_anciliary_qubits = 1
     amnt_qubits = amnt_non_anciliary_qubits + amnt_anciliary_qubits
@@ -52,13 +58,11 @@ def grover(oracle):
     write_result(qc, "2", amnt_non_anciliary_qubits)
     for it in range(iterations):
         qc = oracle(qc, amnt_non_anciliary_qubits, amnt_anciliary_qubits)
-        write_result(qc, str(2*it + 3), amnt_non_anciliary_qubits)
+        write_result(qc, str(2 * it + 3), amnt_non_anciliary_qubits)
         qc = diffusion_operator(qc, amnt_non_anciliary_qubits)
-        write_result(qc, str(2*it + 4), amnt_non_anciliary_qubits)
+        write_result(qc, str(2 * it + 4), amnt_non_anciliary_qubits)
     return
+
 
 if __name__ == "__main__":
     grover(all_one_oracle)
-
-
-
